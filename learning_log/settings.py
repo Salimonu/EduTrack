901391a9d81b7da31ebabcf0857421a973078293
Hammed <salimonuh@gmail.com>
2025-05-12 08:06:19 +0100

    May_12_Modified

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s7crvuhe_ie@i&bu1xak@3nmyeu-zkzd&doldar!02jv*y#-7$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
